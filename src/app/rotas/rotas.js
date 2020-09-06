5c38abafb7ed7e39c0fa75e4e82a5ad04033d037
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-09-06 00:59:02 -0300

    adding db and inserting books into database

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,3 +1,9 @@
+const LivroDao = require('../infra/livro-dao');
+
+const db = require('../../config/database');
+const { d } = require('marko/src/runtime/helpers');
+const listaMarko = require('../views/livros/lista/lista.marko');
+
 module.exports = (app) =>{
 
     app.get('/', function(req,resp){
@@ -15,21 +21,29 @@ module.exports = (app) =>{
     
     
     app.get('/livros', function(req, resp) {
-        resp.marko(
-            require('../views/livros/lista/lista.marko'),
-            {
-                livros: [
-                    { 
-                        id: 1,
-                        titulo: 'Fundamentos do Node'
-                    },
-                    { 
-                        id: 2,
-                        titulo: 'Node Avançado'
-                    }
-                ]
-            }
-    
-        );
+        
+        const livroDao = new LivroDao(db);
+        livroDao.lista()
+            .then(livros => resp.marko(
+                require('../views/livros/lista/lista.marko'),
+                {
+                    livros: livros
+                }
+        
+            ))
+            .catch(erro => console.log(erro)); // se ouver erro imprimir no console
     });
-}
+
+    app.get('/livros/form', function(req,resp){
+        resp.marko(require('../views/livros/form/form.marko'));
+    });
+
+    app.post('/livros', function(req,resp){
+        console.log(req.body);
+
+        const livroDao = new LivroDao(db);
+        livroDao.adiciona(req.body)
+            .then(resp.redirect('/livros'))
+            .catch(erro => console.log(erro)); // se ouver erro imprimir no console
+    })
+};
