93e0880faebb159b6b911c9ad50f9a4f7fa40997
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-09-06 23:55:07 -0300

    adding remotion and editing to database itens

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -19,6 +19,21 @@ module.exports = (app) =>{
         `);
     });
     
+    app.get('/livros/form/:id', function(req, resp) {
+        const id = req.params.id;
+        const livroDao = new LivroDao(db);
+    
+        livroDao.buscaPorId(id)
+            .then(livro => 
+                resp.marko(
+                    require('../views/livros/form/form.marko'),
+                    { livro: livro }
+                )
+            )
+            .catch(erro => console.log(erro));
+    
+    });
+
     
     app.get('/livros', function(req, resp) {
         
@@ -35,7 +50,7 @@ module.exports = (app) =>{
     });
 
     app.get('/livros/form', function(req,resp){
-        resp.marko(require('../views/livros/form/form.marko'));
+        resp.marko(require('../views/livros/form/form.marko'), {livro:{} });
     });
 
     app.post('/livros', function(req,resp){
@@ -45,5 +60,24 @@ module.exports = (app) =>{
         livroDao.adiciona(req.body)
             .then(resp.redirect('/livros'))
             .catch(erro => console.log(erro)); // se ouver erro imprimir no console
-    })
+    });
+
+    app.put('/livros', function(req,resp){
+        console.log(req.body);
+
+        const livroDao = new LivroDao(db);
+        livroDao.atualiza(req.body)
+            .then(resp.redirect('/livros'))
+            .catch(erro => console.log(erro)); // se ouver erro imprimir no console
+    });
+
+    app.delete('/livros/:id', function(req, resp) {
+        const id = req.params.id;
+    
+        const livroDao = new LivroDao(db);
+        livroDao.remove(id)
+            .then(() => resp.status(200).end())
+            .catch(erro => console.log(erro));
+    
+    });
 };
--- src/config/database.js
+++ src/config/database.js
@@ -16,7 +16,7 @@ INSERT INTO usuarios (
     nome_completo, 
     email,
     senha
-) SELECT 'Gabriel Leite', 'gabriel@alura.com.br', '123' WHERE NOT EXISTS (SELECT * FROM usuarios WHERE email = 'gabriel@alura.com.br')
+) SELECT 'anon ', 'anon@alura.com.br', '123' WHERE NOT EXISTS (SELECT * FROM usuarios WHERE email = 'anon@alura.com.br')
 `;
 
 const LIVROS_SCHEMA = 
