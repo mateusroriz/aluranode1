f615c66855c385161c6062bec5c7a0e9aa18cf71
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-09-08 22:11:19 -0300

    adding authentication and authorization to users

--- src/app/controladores/base-controlador.js
+++ src/app/controladores/base-controlador.js
@@ -1,10 +1,13 @@
 const templates = require('../views/template');
+const passport = require('passport');
+const LivroControlador = require('./livro-controlador');
 
 class BaseControlador {
 
     static rotas() {
         return {
-            home: '/'
+            home: '/',
+            login: '/login' 
         };
     }
 
@@ -13,6 +16,36 @@ class BaseControlador {
             resp.marko(
                 templates.base.home
             );
+        };
+    }
+
+    login() {
+
+        return function(req, resp) {
+            resp.marko(templates.base.login);
+        };
+    }
+
+    efetuaLogin() {
+
+        return function(req, resp,next) {
+
+            // Lógica de login.
+            const passport = req.passport;
+            passport.authenticate('local', (erro,usuario, info)=>{
+                if(info){
+                    return resp.marko(templates.base.login);
+                }
+                if (erro){
+                    return next(erro);
+                }
+                req.login(usuario, ()=>{
+                    if(erro){
+                        return next(erro);
+                    }
+                    return resp.redirect(LivroControlador.rotas().lista);
+                });
+            })(req,respo,next);
         };
     }
 }
